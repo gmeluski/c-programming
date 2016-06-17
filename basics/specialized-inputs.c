248c8d3a946ddcc3faae06589f6d2b169d49e26a
Gregg Meluski <gmeluski@hotmail.com>
2016-06-16 18:59:09 -0700

    specialized input example with external variables

---
+++ basics/specialized-inputs.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int getthisline(void);
+void copy(void);
+
+int main (void) {
+  int len;
+  extern int max;
+  extern char longest[];
+
+  max = 0;
+  while ((len = getthisline()) > 0) {
+    if (len > max) {
+      max = len;
+      copy();
+    }
+  }
+
+  if (max > 0) {
+    printf("%s", longest);
+
+  }
+
+  return 0;
+}
+
+int getthisline(void) {
+  int c;
+  int i;
+  extern char line[];
+
+  for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    line[i] = c;
+  }
+
+  if (c == '\n') {
+    line[i] = c;
+    i++;
+  }
+
+  line[i] = '\0';
+
+  return i;
+}
+
+void copy(void) {
+  int i;
+  extern char line[];
+  extern char longest[];
+
+  i = 0;
+  while((longest[i] = line[i]) != '\0') {
+    i++;
+  }
+}
